5afec8b4d7117e6b93f20f49ca5452bb869c8f24
MachikoFujii <machiko.fujii@gmail.com>
2021-05-06 23:21:05 +0900

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,25 +6,4 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to new_session_path unless current_user
   end
-  
-  def authenticate_user
-    if logged_in?
-      unless current_user.id == @posts.user_id
-        flash[:notice] = "権限がありません。"
-        redirect_to posts_path
-      end
-    else
-      redirect_to new_user_path, notice:"ログインが必要です"
-    end
-  end
-  def ensure_current_user
-    if logged_in?
-      unless current_user.id == params[:id].to_i
-        flash[:notice] = "権限がありません。"
-        redirect_to posts_path
-      end
-    else
-      redirect_to new_user_path, notice:"ログインが必要です"
-    end
-  end
 end
